9e9e256877b5c29b8c2f838198ee4857ff8ab103
andrasat <andra.satria1@gmail.com>
2023-07-16 17:38:12 +0700

    formatted with prettier default, run DEBUG with iteration 1e6

--- bun.js
+++ bun.js
@@ -1,72 +1,72 @@
-const crypto = require('node:crypto')
-const util = require('./util')
+const crypto = require("node:crypto")
+const util = require("./util")
 
 Bun.serve({
   port: 3030,
   async fetch(req) {
     const url = new URL(req.url)
-    process.stdout.write(`BUN:${req.method}:${url.pathname}:${new Date().toISOString()}\n`);
+    process.stdout.write(`BUN:${req.method}:${url.pathname}:${new Date().toISOString()}\n`)
 
     switch (url.pathname) {
-      case '/': {
-        const str = 'Healthy'
+      case "/": {
+        const str = "Healthy"
         return new Response(str, {
           headers: {
-            'Content-Length': Buffer.byteLength(str),
-            'Content-Type': 'text/plain'
-          }
+            "Content-Length": Buffer.byteLength(str),
+            "Content-Type": "text/plain",
+          },
         })
       }
-      case '/ping': {
-        const data = JSON.stringify({ data: 'PONG' })
+      case "/ping": {
+        const data = JSON.stringify({ data: "PONG" })
         return new Response(data, {
           headers: {
-            'Content-Length': Buffer.byteLength(data),
-            'Content-Type': 'application/json'
-          }
+            "Content-Length": Buffer.byteLength(data),
+            "Content-Type": "application/json",
+          },
         })
       }
-      case '/blocking': {
-        const hash = crypto.createHash('sha256')
+      case "/blocking": {
+        const hash = crypto.createHash("sha256")
 
         for (let i = 0; i < 1e7; i++) {
           hash.update(util.getRandomString())
         }
 
-        const data = JSON.stringify({ data: hash.digest('hex') })
+        const data = JSON.stringify({ data: hash.digest("hex") })
         return new Response(data, {
           headers: {
-            'Content-Length': Buffer.byteLength(data),
-            'Content-Type': 'application/json'
-          }
+            "Content-Length": Buffer.byteLength(data),
+            "Content-Type": "application/json",
+          },
         })
       }
-      case '/not-blocking': {
-        const hash = crypto.createHash('sha256')
-        const debugIteration = process.env.DEBUG ? 1e3 : 1e7
+      case "/not-blocking": {
+        const hash = crypto.createHash("sha256")
+        const debugIteration = process.env.DEBUG ? 1e6 : 1e7
 
         for (let i = 0; i < debugIteration; i++) {
-          await util.runAsyncSetTimeout1(() => hash.update(util.getRandomString()))
+          await util.runAsyncSetTimeout0(() => hash.update(util.getRandomString()))
         }
 
-        const data = JSON.stringify({ data: hash.digest('hex') })
+        const data = JSON.stringify({ data: hash.digest("hex") })
         return new Response(data, {
           headers: {
-            'Content-Length': Buffer.byteLength(data),
-            'Content-Type': 'application/json'
-          }
+            "Content-Length": Buffer.byteLength(data),
+            "Content-Type": "application/json",
+          },
         })
       }
       default:
-        return new Response('Not found', {
+        return new Response("Not found", {
           status: 404,
           headers: {
-            'Content-Length': Buffer.byteLength('Not found'),
-            'Content-Type': 'text/plain'
-          }
+            "Content-Length": Buffer.byteLength("Not found"),
+            "Content-Type": "text/plain",
+          },
         })
     }
-  }
+  },
 })
 
-process.stdout.write('Server is running on port 3030\n');
+process.stdout.write("Server is running on port 3030\n")
--- util.js
+++ util.js
@@ -1,30 +1,36 @@
-const crypto = require('node:crypto');
+const crypto = require("node:crypto")
 
 function getRandomString() {
-  const stringSize = 20;
-  const randomString = crypto.randomBytes(stringSize).toString();
-  return randomString;
+  const stringSize = 20
+  const randomString = crypto.randomBytes(stringSize).toString()
+  return randomString
 }
 
 function runAsyncSetTimeout1(fn) {
-  return new Promise(resolve => setTimeout(() => {
-    fn()
-    resolve()
-  }, 1))
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      fn()
+      resolve()
+    }, 1)
+  )
 }
 
 function runAsyncSetTimeout0(fn) {
-  return new Promise(resolve => setTimeout(() => {
-    fn()
-    resolve()
-  }, 0))
+  return new Promise((resolve) =>
+    setTimeout(() => {
+      fn()
+      resolve()
+    }, 0)
+  )
 }
 
 function runAsyncSetImmediate(fn) {
-  return new Promise(resolve => setImmediate(() => {
-    fn()
-    resolve()
-  }))
+  return new Promise((resolve) =>
+    setImmediate(() => {
+      fn()
+      resolve()
+    })
+  )
 }
 
 module.exports = {
